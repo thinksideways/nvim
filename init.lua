e6edb74c2ffbfe1858a714487e24f6def323705a
Sam Kaiser <samuel.k.public@gmail.com>
2025-12-22 18:53:52 -0600

    Initial options that usually annoy me right away

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- four space tabs (because this gets annoying immediately)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- enabling page numbers
+vim.opt.number = true
+vim.opt.relativenumber=true
+
+-- always allow mouse support
+vim.opt.mouse = 'a'
+
+-- only search case sensitive when search term has uppercase letter
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- attempt to save undo history from session to session
+vim.opt.undofile = true
+
